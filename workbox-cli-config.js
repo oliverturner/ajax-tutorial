89dcae6a50873b765a6fe030cbd040fa7305fcab
Oliver Turner <oliver.turner@codedsignal.co.uk>
2017-10-23 09:13:12 +0100

    Implemented Workbox and added PWA assets (icons, manifest.json, etc)

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{js,ico,css,html}"
+  ],
+  "swDest": "dist/sw.js",
+  "globIgnores": [
+    "../workbox-cli-config.js"
+  ]
+};
