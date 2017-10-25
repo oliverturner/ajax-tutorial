0bb517f1f5f6a99f71ae280010f49f134cccd676
Oliver Turner <oliver.turner@codedsignal.co.uk>
2017-10-25 18:38:25 +0100

    waypoint

--- tools/manifest.js
+++ tools/manifest.js
@@ -8,22 +8,20 @@ const baseDir = path.resolve(__dirname, "..");
 const { dirs, urls } = pkg.app;
 
 const iconDir = path.resolve(baseDir, dirs.icons);
+const iconFiles = fs.readdirSync(iconDir);
 const iconUrl = `${urls.base}/${urls.icons}`;
 const iconRegex = /(\d{1,3}x\d\d{1,3}).png$/;
 const manifestDest = path.resolve(baseDir, dirs.docRoot, urls.manifest);
 
-const getIcons = (dir, regex, url) =>
-  fs
-    .readdirSync(dir)
-    .filter(icon => regex.exec(icon))
-    .map(file => ({
-      src: `${url}/${file}`,
-      sizes: regex.exec(file)[1],
-      type: "image/png"
-    }));
+const getIcons = (files, regex, url) =>
+  files.filter(icon => regex.exec(icon)).map(file => ({
+    src: `${url}/${file}`,
+    sizes: regex.exec(file)[1],
+    type: "image/png"
+  }));
 
 const getManifest = () => {
-  const icons = getIcons(iconDir, iconRegex, iconUrl);
+  const icons = getIcons(iconFiles, iconRegex, iconUrl);
   return Object.assign({}, pkg.app.manifest, { icons });
 };
 
