9e32ac162acf552c6ca5d3516272f0a29c33b85b
Oliver Turner <oliver.turner@codedsignal.co.uk>
2017-10-27 13:25:31 +0100

    Feature complete for Stage 2: thumbs added; Starting Stage 3

--- app.js
+++ app.js
@@ -1,72 +1,109 @@
 const $body = document.querySelector("body");
-const $btn = document.querySelector("#moar-btn");
 const $img = document.querySelector("#img");
+const $thumbs = document.querySelector("#thumbs");
+const $conditions = document.querySelector("#conditions");
 const $creditUser = document.querySelector("#credit-user");
 const $creditPlatform = document.querySelector("#credit-platform");
 
 const apis = {
   weather: {
-    key: "a70fde74ccea0ae4b9d62142c9dece40"
+    key: "a70fde74ccea0ae4b9d62142c9dece40",
+    url: "http://api.openweathermap.org/data/2.5/weather"
   },
   unsplash: {
     key: "df24b455387acb47127898da32793c0e9b3a43b75af80a857feb17cffe4af7f0",
+    url: "https://api.unsplash.com/photos/",
     utm: "?utm_source=weather&utm_medium=referral&utm_campaign=api-credit"
   }
 };
 
 // Utility methods
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
 function onError(err) {
   console.log(err);
 }
 
+function createThumb(item) {
+  const img = document.createElement("img");
+  img.src = item.urls.thumb;
+  img.className = "thumbs__link__img";
+
+  const a = document.createElement("a");
+  a.href = item.links.html;
+  a.className = "thumbs__link";
+  a.appendChild(img);
+
+  return a;
+}
+
+function createThumbs(json) {
+  const f = document.createDocumentFragment();
+  json.map(function(item) {
+    $thumbs.appendChild(createThumb(item));
+  });
+
+  return f;
+}
+
 // APPLICATION
-// Step 1
-const fetchWeather = () => {
-  const key = apis.weather.key;
-  const url = "http://api.openweathermap.org/data/2.5/weather";
-  const query = "London,uk";
-
-  fetch(url + "?q=" + query + "&appid=" + key)
-    .then(function(res) {
-      return res.json();
-    }, onError)
-    .then(fetchImages)
-    .catch(onError);
-};
-
-// Step 2
-function fetchImages(json) {
-  const key = apis.unsplash.key;
-  const url = "https://api.unsplash.com/photos/";
-  const query = json.weather[0].main;
-
-  fetch(url + "?query=" + query + "&client_id=" + key)
-    .then(res => res.json(), onError)
-    .then(displayImage)
-    .catch(onError);
-}
-
-// Step 3
-function displayImage(json) {
-  const index = getRandomInt(0, json.length);
-  const image = json[index];
-
-  $img.src = image.urls.full;
-  $body.style["backgroundColor"] = image.color;
-  $creditUser.href = image.user.links.html + apis.unsplash.utm;
-  $creditUser.textContent = image.user.name;
-}
-
-// START!
 (function() {
-  $creditPlatform.href = "https://unsplash.com/" + apis.unsplash.utm;
-  $btn.addEventListener("click", fetchWeather);
+  let imageData = [];
+
+  // Step 1
+  const fetchWeather = () => {
+    const key = apis.weather.key;
+    const url = apis.weather.url;
+    const query = "London,uk";
+
+    fetch(url + "?q=" + query + "&appid=" + key)
+      .then(function(res) {
+        return res.json();
+      }, onError)
+      .then(fetchImages)
+      .catch(onError);
+  };
+
+  // Step 2
+  function fetchImages(json) {
+    const key = apis.unsplash.key;
+    const url = apis.unsplash.url;
+    const query = json.weather[0].main;
+
+    $conditions.textContent = query;
+
+    fetch(url + "?query=" + query + "&client_id=" + key)
+      .then(res => res.json(), onError)
+      .then(onImageData)
+      .catch(onError);
+  }
+
+  // Step 3
+  function onImageData(json) {
+    imageData = json;
+    $thumbs.appendChild(createThumbs(imageData));
+    displayImage(0);
+  }
+
+  // Step 4
+  function displayImage(index) {
+    const image = imageData[index];
+    $img.src = image.urls.regular;
+    $body.style.backgroundColor = image.color;
+    $creditUser.href = image.user.links.html + apis.unsplash.utm;
+    $creditUser.textContent = image.user.name;
+  }
+
+  // Event handlers
+  function onThumbClick(event) {
+    if (event.target.matches("a")) {
+      event.preventDefault();
+      const links = Array.from($thumbs.children);
+      const index = links.indexOf(event.target);
+      displayImage(index);
+    }
+  }
+
+  // START!
+  $thumbs.addEventListener("click", onThumbClick);
+  $creditPlatform.href = "https://unsplash.com" + apis.unsplash.utm;
   fetchWeather();
 })();
