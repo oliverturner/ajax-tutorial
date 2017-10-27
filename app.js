f07b0630347791bc9cd64591b52ee51d269910f8
Oliver Turner <oliver.turner@codedsignal.co.uk>
2017-10-27 15:24:36 +0100

    Temporarily replaced code with improved ES5

--- app.js
+++ app.js
@@ -1,190 +1,109 @@
-const getConfig = ({ weather, unsplash }) => {
-  console.assert(weather.apiKey, "must supply weather.apiKey");
-  console.assert(weather.city, "must supply weather.city");
-  console.assert(unsplash.apiKey, "must supply unsplash.apiKey");
-  console.assert(unsplash.appName, "must supply unsplash.appName");
+const $body = document.querySelector("body");
+const $img = document.querySelector("#img");
+const $thumbs = document.querySelector("#thumbs");
+const $conditions = document.querySelector("#conditions");
+const $creditUser = document.querySelector("#credit-user");
+const $creditPlatform = document.querySelector("#credit-platform");
 
-  return {
-    weather: {
-      key: weather.apiKey,
-      defaultQuery: weather.city,
-      url: "https://api.openweathermap.org/data/2.5/weather"
-    },
-    unsplash: {
-      key: unsplash.apiKey,
-      utm: `utm_source=${unsplash.appName}&utm_medium=referral&utm_campaign=api-credit`,
-      url: "https://api.unsplash.com/search/photos"
-    }
-  };
+const apis = {
+  weather: {
+    key: "a70fde74ccea0ae4b9d62142c9dece40",
+    url: "http://api.openweathermap.org/data/2.5/weather"
+  },
+  unsplash: {
+    key: "df24b455387acb47127898da32793c0e9b3a43b75af80a857feb17cffe4af7f0",
+    url: "https://api.unsplash.com/photos/",
+    utm: "?utm_source=weather&utm_medium=referral&utm_campaign=api-credit"
+  }
 };
 
 // Utility methods
-//------------------------------------------------------------------------------
-const clearChildren = parent => {
-  Array.from(parent.children).forEach(el => parent.removeChild(el));
-};
+function onError(err) {
+  console.log(err);
+}
 
-// Check that the server has returned a 200 status
-// (Unlike $.ajax, fetch doesn't treat error messages from the server as failed responses)
-const checkRes = response => {
-  if (!response.ok) {
-    throw Error(response.statusText);
+function createThumb(item) {
+  const img = document.createElement("img");
+  img.src = item.urls.thumb;
+  img.className = "thumbs__link__img";
+
+  const anchor = document.createElement("a");
+  anchor.href = item.links.html;
+  anchor.className = "thumbs__link";
+  anchor.appendChild(img);
+
+  return anchor;
+}
+
+function createThumbs(json) {
+  const fragment = document.createDocumentFragment();
+  json.map(function(item) {
+    $thumbs.appendChild(createThumb(item));
+  });
+
+  return fragment;
+}
+
+// APPLICATION
+(function() {
+  let imageData = [];
+
+  // Step 1
+  const fetchWeather = () => {
+    const key = apis.weather.key;
+    const url = apis.weather.url;
+    const query = "London,uk";
+
+    fetch(url + "?q=" + query + "&appid=" + key)
+      .then(function(res) {
+        return res.json();
+      }, onError)
+      .then(fetchImages)
+      .catch(onError);
+  };
+
+  // Step 2
+  function fetchImages(json) {
+    const key = apis.unsplash.key;
+    const url = apis.unsplash.url;
+    const query = json.weather[0].main;
+
+    $conditions.textContent = query;
+
+    fetch(url + "?query=" + query + "&client_id=" + key)
+      .then(res => res.json(), onError)
+      .then(onImageData)
+      .catch(onError);
   }
 
-  return response;
-};
+  // Step 3
+  function onImageData(json) {
+    imageData = json;
+    $thumbs.appendChild(createThumbs(imageData));
+    displayImage(0);
+  }
 
-//------------------------------------------------------------------------------
-// Main
-//------------------------------------------------------------------------------
-function App(userConfig) {
-  // Cache references to DOM elements
-  const $body = document.querySelector("body");
-  const $thumbs = document.querySelector("#thumbs");
-  const $photo = document.querySelector("#photo");
-  const $search = document.querySelector("#search");
-  const $searchField = document.querySelector("#search-tf");
-  const $creditUser = document.querySelector("#credit-user");
-  const $creditPlatform = document.querySelector("#credit-platform");
+  // Step 4
+  function displayImage(index) {
+    const image = imageData[index];
+    $img.src = image.urls.regular;
+    $body.style.backgroundColor = image.color;
+    $creditUser.href = image.user.links.html + apis.unsplash.utm;
+    $creditUser.textContent = image.user.name;
+  }
 
-  // Merge user-supplied values (api keys, etc.) into config
-  const config = getConfig(userConfig);
-
-  const createThumb = (parent, src, alt) => {
-    const img = document.createElement("img");
-    img.src = src;
-    img.alt = alt;
-    img.className = "thumbs__thumb";
-    img.addEventListener("load", () => parent.appendChild(img));
-  };
-
-
-  const onLinkClick = (term, image) => e => {
-    e.preventDefault();
-    displayMain(term, image);
-  };
-
-  const displayThumbs = (term, images) => {
-    clearChildren($thumbs);
-
-    // We could use createDocumentFragment and event Delegation for perf
-    // improvements... but for simplicity we append children directly to $thumbs
-    // and add click event handlers directly
-    images.forEach((image, index) => {
-      const url = image.links.html;
-      const alt = image.description || term;
-      const thumbUrl = image.urls.thumb;
-
-      const anchor = document.createElement("a");
-      anchor.href = `${url}?${config.unsplash.utm}`;
-      anchor.className = "thumbs__link";
-      anchor.addEventListener("click", onLinkClick(term, images[index]));
-
-      createThumb(anchor, thumbUrl, alt);
-
-      $thumbs.appendChild(anchor);
-    });
-  };
-
-  // Clear the main image and load the supplied url
-  // Only mount the image once the file has loaded to let it fade in nicely
-  const updatePhoto = (url, alt) => {
-    clearChildren($photo);
-
-    const img = document.createElement("img");
-    img.addEventListener("load", () => {
-      // Take care of any async overruns (ghetto but effective!)
-      clearChildren($photo);
-      $photo.appendChild(img)
-    });
-    img.src = url;
-    img.alt = alt;
-  };
-
-  // Update the main UI
-  const displayMain = (term, image) => {
-    const { utm } = config.unsplash;
-    const { user, urls, color, description } = image;
-
-    const alt = description || term;
-    const imageUrl = urls.regular;
-
-    $body.style["backgroundColor"] = color;
-    $creditUser.href = `${user.links.html}?${utm}`;
-    $creditUser.innerText = `"${term}" by ${user.name}`;
-
-    updatePhoto(imageUrl, alt);
-  };
-
-  // LOADING
-  //----------------------------------------------------------------------------
-  // Step 1/4: Initialise data loading
-  const getCityWeather = query => {
-    fetchCityWeather(query)
-      .then(fetchCityWeatherImages)
-      .then(displayCityWeatherImages)
-      .catch(err => {
-        console.log("getCityWeather:", err);
-      });
-  };
-
-  // Step 2/4: Load weather data for the given city
-  const fetchCityWeather = query => {
-    const { key, url } = config.weather;
-    const endpoint = `${url}?q=${query}&appid=${key}`;
-
-    return fetch(endpoint)
-      .then(checkRes)
-      .then(res => res.json());
-  };
-
-  // Step 3/4: Load derived data (images that match the weather description)
-  const fetchCityWeatherImages = json => {
-    const { key, url } = config.unsplash;
-    const term = json.weather[0].description;
-    const endpoint = `${url}?query=${term}&client_id=${key}`;
-
-    return fetch(endpoint)
-      .then(checkRes)
-      .then(res => res.json())
-      .then(data => ({ term, images: data.results }));
-  };
-
-  // Step 4/4: All data loaded...
-  // Call the methods that build the UI
-  const displayCityWeatherImages = ({ term, images }) => {
-    displayThumbs(term, images);
-    displayMain(term, images[0]);
-  };
-
-  const onSearch = e => {
-    e.preventDefault();
-
-    // Only search when the term is valid
-    const term = $search.term.value;
-    if (term.length) {
-      getCityWeather(term);
+  // Event handlers
+  function onThumbClick(event) {
+    if (event.target.matches("a")) {
+      event.preventDefault();
+      const links = Array.from($thumbs.children);
+      const index = links.indexOf(event.target);
+      displayImage(index);
     }
-  };
+  }
 
-  // INITIALISE
-  //----------------------------------------------------------------------------
-  const initCredits = utm => {
-    $creditPlatform.href = `https://unsplash.com/?${utm}`;
-  };
-  const initSearch = () => {
-    $search.addEventListener("submit", onSearch);
-    $search.term.value = config.weather.defaultQuery;
-    $searchField.addEventListener("focus", () => ($searchField.value = ""));
-  };
-
-  // Grab DOM references and add event handlers
-  const initUI = () => {
-    initCredits(config.unsplash.utm);
-    initSearch();
-  };
-
-  // Bootstrap UI and autoload first image
-  initUI();
-  getCityWeather(config.weather.defaultQuery);
-}
+  // START!
+  $thumbs.addEventListener("click", onThumbClick);
+  $creditPlatform.href = "https://unsplash.com" + apis.unsplash.utm;
+  fetchWeather();
+})();
